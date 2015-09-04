9fd4defc2aed0a5d8d3261b80aa8c02057e28c84
Inno Estrera <innoirvinge@gmail.com>
2015-09-04 12:14:43 -0700

    Implemented wolfram alpha input retrieval

--- src/WolframApiResponseXML.java
+++ src/WolframApiResponseXML.java
@@ -1,10 +1,20 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import jdk.nashorn.internal.objects.NativeJava;
+import org.w3c.dom.*;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+//import javax.xml.parsers.*;
 
 /**
  * Created by inno.estrera on 9/3/15.
@@ -47,7 +57,7 @@ public class WolframApiResponseXML {
         }
     }
 
-    private String responseMessage(final String url) {
+    public String responseAsString(final String url) {
         try {
             final URL urlObject = getUrlObject(url);
             HttpURLConnection con = (HttpURLConnection) urlObject.openConnection();
@@ -62,8 +72,27 @@ public class WolframApiResponseXML {
             return response.toString();
         } catch(final IOException e) {
             e.printStackTrace();
-            return null;
         }
+        return null;
+    }
+
+    public Document responseAsDocument(final String url) {
+        final String result = responseAsString(url);
+        return convertStringXMLToDocument(result);
+    }
+
+    public Document convertStringXMLToDocument(final String xml) {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder;
+        try {
+            builder = factory.newDocumentBuilder();
+            InputSource inputSource = new InputSource();
+            inputSource.setCharacterStream(new StringReader(xml));
+            return builder.parse(inputSource);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private Object responseContent(final String url) {
@@ -78,22 +107,59 @@ public class WolframApiResponseXML {
         }
     }
 
-    public void retrieveResults(final String input) {
+    public String retrieveResultsAsString(final String input) {
         String urlAsString = _wolframEncoder.updateInputInUrl(input);
-        URL url = getUrlObject(urlAsString);
-        HttpURLConnection connection = getConnection(url);
+        return responseAsString(urlAsString);
+    }
+
+    public Document retrieveResultsAsDocument(final String input) {
+        String urlAsString = _wolframEncoder.updateInputInUrl(input);
+        return responseAsDocument(urlAsString);
+    }
+
+    public List<String> parseSolutionFromXml(final Document xmlDocument) {
+        final List<String> results = new ArrayList<String>();
+
+        NodeList list = xmlDocument.getElementsByTagName("pod");
+        String tobeAdded;
+        for (int i = 0; i < list.getLength(); i++) {
+            Node node = list.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element el = (Element) node;
+                tobeAdded = el.getTextContent().trim();
+                if (tobeAdded.length() >= 1) {
+                    results.add(tobeAdded);
+                }
+            }
+        }
+        return results;
+    }
+
+    public List<String> getSolutionFromWolfram(final String input) {
+        final Document xmlDocument = retrieveResultsAsDocument(input);
+        return parseSolutionFromXml(xmlDocument);
+    }
+
+    public void printDocument(final Document doc) {
+        TransformerFactory tf = TransformerFactory.newInstance();
+        try {
+            System.out.println("======================");
+            System.out.println("======================");
+            System.out.println("======================");
+            Transformer transformer = tf.newTransformer();
+            transformer.transform(new DOMSource(doc), new StreamResult(new OutputStreamWriter(System.out, "UTF-8")));
+            System.out.println("======================");
+            System.out.println("======================");
+            System.out.println("======================");
+        } catch(final Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(final String... args) {
         WolframApiResponseXML test = new WolframApiResponseXML();
-//        test.retrieveResults("Inno pot pot");
-        final URL url = test.getUrlObject("http://localhost:3000");
-        final int result = test.responseCode("http://localhost:3000");
-        final String response = test.responseMessage("http://localhost:3000");
-        final Object content = test.responseContent("http://localhost:3000");
-
-        System.out.println("result: " + result);
-        System.out.println("message: " + response);
-        System.out.println("content: " + content);
+        final String input = "x^2+2x+1=234";
+        final List<String> results = test.getSolutionFromWolfram(input);
+        System.out.println(results);
     }
 }
